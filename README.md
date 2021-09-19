97b4e55070c8789533c8baa3626b560669aad802
Peter  Lu <peterlu0928@protonmail.com>
2021-09-19 23:58:36 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,16 @@
+This script helps you to download Ehco to you local machine (currenly amd64 linux supported, I hard coded it cuz I'm lazy).
+
+To use this script:
+```
+bash <(wget https://raw.githubusercontent.com/Mr-Sheep/ehcoInstall/master/ehcoInstall.sh)
+
+```
+
+This script will create:
+```
+/root/config.json
+/usr/local/bin/ehco
+/lib/systemd/system/ehco.service
+```
+
+
---
+++ ehco.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+packageLink=$(curl -s https://api.github.com/repos/Ehco1996/ehco/releases/latest | grep "browser_download_url.*linux_amd64" | cut -d '"' -f 4 | wget -i -)
+packageName=$(curl -s https://api.github.com/repos/Ehco1996/ehco/releases/latest | grep "browser_download_url.*linux_amd64" | cut -d '"' -f 4 | cut -d "/" -f 9)
+
+configURL=$(wget -O "/root/config.json" https://raw.githubusercontent.com/Mr-Sheep/ehcoInstall/master/config.json)
+systemd=$(wget -O "/lib/systemd/system/ehco.service" https://raw.githubusercontent.com/Mr-Sheep/ehcoInstall/master/ehco.service )
+
+chmod +x $packageName && mv $packageName /usr/local/bin/ehco
+
+if $(ehco -v 2>/dev/null); then
+  echo -e "\n ${GREEN} Ehco's installtion process completed ${NC}"
+else
+  echo -e "\n ${RED} ERROR, try install ehco manually ${NC}"
+fi
